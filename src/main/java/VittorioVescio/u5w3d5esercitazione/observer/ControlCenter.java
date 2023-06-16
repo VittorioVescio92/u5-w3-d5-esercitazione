4fca09c86990491dffb1d5e0bba45bb6a8720f8f
VittorioVescio92 <123975369+VittorioVescio92@users.noreply.github.com>
2023-06-16 13:17:30 +0200

    Aggiunti nuovi sensori

--- src/main/java/VittorioVescio/u5w3d5esercitazione/observer/ControlCenter.java
+++ src/main/java/VittorioVescio/u5w3d5esercitazione/observer/ControlCenter.java
@@ -4,18 +4,21 @@ public class ControlCenter implements Observer {
 
 	@Override
 	public void update(FireSensor fs) {
-		if (fs.getSmokeLevel() > 5) {
+		String url = "http://host/alarm?fireSensorId=" + fs.getFireSensorId() + "&lat=" + fs.getLatitudine() + "&lon="
+				+ fs.getLongitudine() + "&smokelevel=" + fs.getSmokeLevel();
 
-			String url = "http://host/alarm?fireSensorId=" + fs.getFireSensorId() + "&lat=" + fs.getLatitudine()
-					+ "&lon=" + fs.getLongitudine() + "&smokelevel=" + fs.getSmokeLevel();
-			System.out.println();
-			System.out.println("Allarme, incendio in corso: " + url);
-			System.out.println();
+		if (fs.getSmokeLevel() > 5) {
+			if (url.contains("lat=41.871857&lon=12.477398")) {
+				System.out.println();
+				System.out.println(
+						"Attenzione, gli uffici di Epicode sono in fiamme! Fare Commit + Push prima di abbandonare l'edificio!");
+			} else {
+				System.out.println();
+				System.out.println("Allarme! Incendio in corso: " + url);
+			}
 		} else {
 			System.out.println();
 			System.out.println("Tutto ok, livelli di fumo nella norma!");
-			System.out.println();
 		}
 	}
-
 }
--- src/main/java/VittorioVescio/u5w3d5esercitazione/observer/ObserverRunner.java
+++ src/main/java/VittorioVescio/u5w3d5esercitazione/observer/ObserverRunner.java
@@ -8,13 +8,19 @@ public class ObserverRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		FireSensor fireSensor1 = new FireSensor(109.94, 7.30);
+		FireSensor fireSensor1 = new FireSensor(-7.418047, 110.130657); // easter egg :D
+		FireSensor fireSensor2 = new FireSensor(41.871857, 12.477398); // easter egg :D
+		FireSensor fireSensor3 = new FireSensor(15.584632, 100.785413);
 		ControlCenter controlCenter = new ControlCenter();
 
 		fireSensor1.addObserver(controlCenter);
+		fireSensor2.addObserver(controlCenter);
+		fireSensor3.addObserver(controlCenter);
 
-		fireSensor1.setSmokeLevel(3);
+		fireSensor1.setSmokeLevel(5);
 		fireSensor1.setSmokeLevel(7);
+		fireSensor2.setSmokeLevel(10);
+		fireSensor3.setSmokeLevel(11);
 	}
 
 }
